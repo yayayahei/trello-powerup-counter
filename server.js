c90ac40df1426ad3ddf0b565b9e97178200ecb31
yayayahei <mmwcbz@msn.cn>
2018-10-10 23:07:37 +0800

    init

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+// where your node app starts
+// require('dotenv').config();
+var dotenv=require('dotenv');
+var compression = require('compression');
+var cors = require('cors');
+var express = require('express');
+var nocache = require('node-nocache');
+var http=require('http');
+
+const result = dotenv.config()
+ 
+if (result.error) {
+  throw result.error
+}
+ 
+console.log(result.parsed)
+
+var app = express();
+
+// compress our client side content before sending it over the wire
+app.use(compression());
+
+// your manifest must have appropriate CORS headers, you could also use '*'
+app.use(cors({ origin: 'https://trello.com' }));
+
+// https://github.com/mingchen/node-nocache
+app.use('/manifest.json', nocache, function (request, response) {
+  response.sendFile(__dirname + '/public/manifest.json');
+});
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+// var server = http.createServer(app);
+// server.listen(80,'0.0.0.0');
+
+http.createServer(app).listen(process.env.PORT, 'localhost');
+// console.log('after express.start')
+// // listen for requests :)
+// var listener = app.listen(process.env.PORT, function () {
+//   console.info(`Node Version: ${process.version}`);
+//   console.log('Trello Power-Up Server listening on port ' + listener.address().port);
+// });
+
